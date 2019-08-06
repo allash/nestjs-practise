772edafbf08530fc638ba5e5ba45779c23b266da
allash <1172129+allash@users.noreply.github.com>
2019-08-06 14:19:35 +0200

    [IMPROVEMENT] enable cors

--- nestjs-1/src/main.ts
+++ nestjs-1/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,18 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/swagger', app, document);
+
+  // init cors
+  const corsOptions: CorsOptions = {
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-xsrf-token', 'x-auth-token'],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: ['*'],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  };
+
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.PORT || 3001);
 }
