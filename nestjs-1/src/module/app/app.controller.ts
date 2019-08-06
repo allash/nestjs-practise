bba5677e25425a198e8e6960e566a627007d1edf
allash <1172129+allash@users.noreply.github.com>
2019-08-06 11:53:56 +0200

    [IMPROVEMENT] small change

--- nestjs-1/src/module/app/app.controller.ts
+++ nestjs-1/src/module/app/app.controller.ts
@@ -11,6 +11,6 @@ export class AppController extends BaseController {
   @Public()
   @Get('/ping')
   ping(): string {
-    return 'pong from circleci master';
+    return 'pong from circleci master!';
   }
 }
